99d5cd982d58aa72fb7dcdde40d940588cbfc44d
juhyoungRyu <thinkoutside2004@gmail.com>
2021-07-30 21:37:12 +0900

    20210730

--- js/todo.js
+++ js/todo.js
@@ -13,17 +13,20 @@ function saveToDos() {
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     const button = document.createElement("button");
     button.innerText = "X";
     button.addEventListener("click", deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     toDoList.appendChild(li);
 }
 
@@ -31,8 +34,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text :newTodo,
+        id : Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
@@ -44,4 +51,8 @@ if(savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
     toDos = parsedToDos;
     parsedToDos.forEach(paintTodo);  
+}
+
+function sexyFilter() {
+
 }
