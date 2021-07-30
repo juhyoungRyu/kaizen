03415909bad80235c47398fa27d6fb351d011f55
juhyoungRyu <thinkoutside2004@gmail.com>
2021-07-30 14:59:22 +0900

    20210730

--- js/todo.js
+++ js/todo.js
@@ -1,17 +1,25 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
-
 const toDoList = document.getElementById("todo-list");
+
+function deleteTodo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "X";
+    button.addEventListener("click",deleteTodo);
     li.appendChild(span);
+    li.appendChild(button);
     span.innerText = newTodo;
-    toDoList.appendChild(li); 
+    toDoList.appendChild(li);
 }
 
-function handleToDoSubmit() {
+function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
