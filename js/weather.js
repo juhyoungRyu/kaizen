d9215935ffa33b2b5a3bb0a0032af00db69d14a8
juhyoungRyu <thinkoutside2004@gmail.com>
2021-07-31 23:22:25 +0900

    20210731

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,10 @@ const API_KEY = "203a7ce25e898c0f80c7e8316fb90304"
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
-    console.log(url);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json().then(data => {
+        console.log(data.name, data.weather[0].main);
+    }));
 }
 
 function onGeoError() {
@@ -13,4 +15,3 @@ function onGeoError() {
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 
-fetch(url);
