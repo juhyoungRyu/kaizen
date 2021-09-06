2f2fc5675564fb2d163eb2d3f8e2dba8dc44b213
juhyoungRyu <thinkoutside2004@gmail.com>
2021-09-06 21:32:34 +0900

    210906

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,21 +1,28 @@
-import dummy from "../db/data.json";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import Word from "./Word";
 
 export default function Day() {
 
     const { day } = useParams();
-    const wordList = dummy.words.filter(word => (
-        word.day === Number(day)
-    ))
+    const [words, setWords] = useState([])
 
+    useEffect(()=>{
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setWords(data)
+        })
+    }, [day])
 
     return( 
     <>
     <h2>Day {day}</h2>
         <table>
             <tbody>
-                {wordList.map(word => (
+                {words.map(word => (
                     <Word word={word} key={word.id} />
                 ))}
             </tbody>
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,30 @@
+import { useEffect,useState } from 'react';
 import {Link} from 'react-router-dom';
-import dummy from "../db/data.json"
 
 export default function DayList() {
-    return <ul className="list_day">
-        {dummy.days.map(day => (
-            <li key={day.id}>
-                <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-            </li>
-        ))}
-    </ul>
+    const [days, setDays] = useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:3001/days")
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            setDays(data)
+        })
+    }, [])
+
+    return(
+        <> 
+            <ul className="list_day">
+                {days.map(day => (
+                    <li key={day.id}>
+                        <Link to={`/day/${day.day}`}>
+                            Day {day.day}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
