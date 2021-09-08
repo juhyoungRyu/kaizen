27a864dff1e47ae10eee1ab9dfff80f4e2ffe2bc
juhyoungRyu <thinkoutside2004@gmail.com>
2021-09-09 00:04:06 +0900

    베르트랑 공준

--- 9_basic_math2/5_4948.py
+++ 9_basic_math2/5_4948.py
@@ -2,12 +2,13 @@ def num(num):
     if num == 1:
         return False
     else:
-        for i in range(2, int(num ** 0.5)+1):
-            if num % i == 0 :
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
                 return False
         return True
 
-all_list = list(range(2,246912))
+
+all_list = list(range(2, 246912))
 save_list = []
 
 for i in all_list:
