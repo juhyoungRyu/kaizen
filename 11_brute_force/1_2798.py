b64f9c83753e564de55ded26acc17014fc88dc3a
juhyoungRyu <thinkoutside2004@gmail.com>
2021-09-19 01:03:58 +0900

    브루트포스-블랙잭

---
+++ 11_brute_force/1_2798.py
@@ -0,0 +1,16 @@
+n, m = map(int, (input().split()))
+d = list(map(int, input().split()))
+
+result = 0
+Max = 0
+
+for i in range(n - 2):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
+            if d[i] + d[j] + d[k] > m:
+                continue
+            else:
+                result = d[i] + d[j] + d[k]
+                if Max <= result:
+                    Max = result
+print(Max)
