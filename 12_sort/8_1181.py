97f1c31a3ebed8ac572ba76c4b360adf669afdaf
juhyoungRyu <thinkoutside2004@gmail.com>
2021-10-03 10:41:48 +0900

    단어 배열 - 코드 최소화

--- 12_sort/8_1181.py
+++ 12_sort/8_1181.py
@@ -4,10 +4,9 @@ word = []
 for _ in range(n):
     word.append(input())
 
-set_word = set(word)
-word = list(set_word)
+word = list(set(word))
 word.sort()
-word.sort(key = len)
+word.sort(key=len)
 
 for i in word:
     print(i)
