4aff8e2c1bc9c58ea2df54de9991537f41738cf4
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-02 12:24:21 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { color } from "./color";
 
 export default function App() {
+  useEffect(() => {
+    console.log(money);
+  }, [money]);
+
+  const [money, setMoney] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.inputCon}>
+        <TextInput
+          placeholder="0₩"
+          value={money}
+          onChangeText={(text) => setMoney(text)}
+          keyboardType="numeric"
+          returnKeyType="done"
+        />
+      </View>
+      <View style={styles.btnCon}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnText}>Next</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +40,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: color.theme3,
+  },
+  inputCon: {
+    flex: 0.8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btnCon: {
+    flex: 0.2,
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  input: {},
+  btn: {
+    padding: 10,
+    backgroundColor: color.theme1,
+    width: "25%",
+    borderRadius: 15,
+    marginRight: 20,
+  },
+  btnText: {
+    color: color.theme2,
+    textAlign: "center",
   },
 });
