057fdaf3afc876794c39f462655c76302138a502
juhyoungryu <thinkoutside2004@gmail.com>
2022-01-03 02:01:07 +0900

    ui구성 완료, 기능 개발 완료, AsyncStorage 준비 중

--- App.js
+++ App.js
@@ -9,24 +9,33 @@ import {
   View,
   TextInput,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { color } from "./color";
 
 export default function App() {
-  const [money, setMoney] = useState("1,000,000");
   const [month, setMonth] = useState(0);
   const [day, setDay] = useState(0);
-  const [number, setNumber] = useState(0);
+
+  const [money, setMoney] = useState(1000000);
   const [minus, setMinus] = useState(0);
+
+  const [coffeMoney, setCoffeMoney] = useState(0);
+  const [handMoney, setHandMoney] = useState(0);
+
+  const [coffeNumber, setCoffeNumber] = useState(0);
+  const [handNumber, setHandNumber] = useState(0);
 
   const handlePlus = (kind) => {
     let m = 0;
 
     if (kind == "coffe") {
       m = 1500;
+      setCoffeMoney(coffeMoney + m);
       setMinus(minus + m);
     } else if (kind == "hand") {
       m = 5000;
+      setHandMoney(handMoney + m);
       setMinus(minus + m);
     }
   };
@@ -36,19 +45,58 @@ export default function App() {
 
     if (kind == "coffe") {
       m = 1500;
+      setCoffeMoney(coffeMoney - m);
       setMinus(minus - m);
     } else if (kind == "hand") {
       m = 5000;
+      setHandMoney(handMoney - m);
       setMinus(minus - m);
     }
   };
 
+  const refreshMoney = () => {
+    setMoney(money - minus);
+    setCoffeMoney(0);
+    setCoffeNumber(0);
+    setHandMoney(0);
+    setHandNumber(0);
+    setMinus(0);
+  };
+
+  const getTime = () => {
+    const now = new Date();
+    setMonth(now.getMonth() + 1);
+    setDay(now.getDate());
+  };
+
+  setInterval(getTime, 43200000);
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.nav}>
+        <TouchableOpacity
+          style={{ padding: 25 }}
+          onPress={() => {
+            Alert.alert("금액 재설정", "금액을 재설정 하시겠습니까?", [
+              {
+                text: "Cancel",
+              },
+              {
+                text: "OK",
+                onPress: () => {
+                  setMoney(380000);
+                },
+              },
+            ]);
+          }}
+        >
+          <Ionicons name="refresh" size={28} color="white" />
+        </TouchableOpacity>
+      </View>
       <View style={styles.moneyView}>
         <Text style={styles.text1}>목표 금액까지</Text>
-        <Text style={styles.text2}>{money}₩</Text>
+        <Text style={styles.text2}>{money.toLocaleString()}₩</Text>
       </View>
       <View style={styles.btnView}>
         <View style={styles.dayCon}>
@@ -63,7 +111,7 @@ export default function App() {
               size={45}
               color={color.theme2}
               style={{
-                width: "20%",
+                width: "25%",
                 textAlign: "center",
               }}
             />
@@ -71,19 +119,19 @@ export default function App() {
               <TouchableOpacity
                 style={styles.touch}
                 onPress={() => {
-                  if (number >= 1) {
-                    setNumber(number - 1);
+                  if (coffeNumber >= 1) {
+                    setCoffeNumber(coffeNumber - 1);
                     handleMinus("coffe");
                   }
                 }}
               >
                 <AntDesign name="minuscircleo" size={20} color="black" />
               </TouchableOpacity>
-              <Text style={{ fontSize: 33 }}>{number}</Text>
+              <Text style={{ fontSize: 33 }}>{coffeNumber}</Text>
               <TouchableOpacity
                 style={styles.touch}
                 onPress={() => {
-                  setNumber(number + 1);
+                  setCoffeNumber(coffeNumber + 1);
                   handlePlus("coffe");
                 }}
               >
@@ -91,7 +139,7 @@ export default function App() {
               </TouchableOpacity>
             </View>
             <View style={styles.minusMoney}>
-              <Text style={styles.text3}>{minus}₩</Text>
+              <Text style={styles.text3}>{coffeMoney}₩</Text>
             </View>
           </View>
 
@@ -101,7 +149,7 @@ export default function App() {
               size={45}
               color={color.theme2}
               style={{
-                width: "20%",
+                width: "25%",
                 textAlign: "center",
               }}
             />
@@ -109,19 +157,19 @@ export default function App() {
               <TouchableOpacity
                 style={styles.touch}
                 onPress={() => {
-                  if (number >= 1) {
-                    setNumber(number - 1);
+                  if (handNumber >= 1) {
+                    setHandNumber(handNumber - 1);
                     handleMinus("hand");
                   }
                 }}
               >
                 <AntDesign name="minuscircleo" size={20} color="black" />
               </TouchableOpacity>
-              <Text style={{ fontSize: 33 }}>{number}</Text>
+              <Text style={{ fontSize: 33 }}>{handNumber}</Text>
               <TouchableOpacity
                 style={styles.touch}
                 onPress={() => {
-                  setNumber(number + 1);
+                  setHandNumber(handNumber + 1);
                   handlePlus("hand");
                 }}
               >
@@ -129,10 +177,28 @@ export default function App() {
               </TouchableOpacity>
             </View>
             <View style={styles.minusMoney}>
-              <Text style={styles.text3}>{minus}₩</Text>
+              <Text style={styles.text3}>{handMoney}₩</Text>
             </View>
           </View>
         </View>
+
+        <TouchableOpacity
+          style={styles.minusBtnCon}
+          onPress={() => {
+            refreshMoney();
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: color.theme1,
+              padding: 15,
+              borderRadius: 10,
+              width: "30%",
+            }}
+          >
+            <Text style={styles.minusBtn}>-{minus}₩</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -144,15 +210,24 @@ const styles = StyleSheet.create({
     backgroundColor: color.theme2,
   },
   moneyView: {
-    flex: 4,
-    justifyContent: "center",
+    flex: 2,
+    justifyContent: "flex-start",
     alignItems: "center",
+    marginBottom: 20,
+  },
+  nav: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    marginRight: 10,
+    marginTop: 10,
   },
   btnView: {
     flex: 6,
     backgroundColor: color.theme3,
     borderTopLeftRadius: 15,
     borderTopRightRadius: 15,
+    marginTop: -30,
   },
   dayCon: {
     flex: 0.8,
@@ -164,7 +239,7 @@ const styles = StyleSheet.create({
     // backgroundColor: "red",
   },
   btnCon: {
-    flex: 5.2,
+    flex: 3.7,
     alignItems: "center",
   },
   itemCon: {
@@ -196,6 +271,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   text3: {
-    fontSize: 25,
+    fontSize: 20,
+  },
+  minusBtnCon: {
+    flex: 1.5,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  minusBtn: {
+    textAlign: "center",
+    color: "black",
   },
 });
