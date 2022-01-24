89308c3a3a7aab7429ca50efaa42eb96b07e38c7
juhyoungryu <thinkoutside2004@gamil.com>
2022-01-24 17:50:44 +0900

    효율적인 화폐 구성 문제 풀이

---
+++ DP/효율적인_화폐_구성.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+money_value = []
+
+for _ in range(n):
+    money_value.append(int(input()))
+
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(money_value[i], m + 1):
+        if d[j - money_value[i]] != 10001:
+            d[j] = min(d[j], d[j - money_value[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
+a
