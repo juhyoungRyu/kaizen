68e9670f85992d1bfd83e7af56f41d5df60638a3
juhyoungRyu <thinkoutside2004@gamil.com>
2022-01-24 23:53:55 +0900

    병사 배치하기 나만의 풀이

--- DP/병사_배치하기.py
+++ DP/병사_배치하기.py
@@ -4,25 +4,25 @@ from array import array
 n = int(input())
 arr = list(map(int, input().split()))
 
-# d = [0] * (n+1)
-# d[0] = arr[0]
-# cnt = 0
+d = [0] * (n+1)
+d[0] = arr[0]
+cnt = 0
 
-# for i in range(1,  n-1):
-#     if d[i-1] + arr[i] > d[i-1] + arr[i+1]:
-#         d[i] = d[i - 1] + arr[i]
+for i in range(1,  n-1):
+    if d[i-1] + arr[i] > d[i-1] + arr[i+1]:
+        d[i] = d[i - 1] + arr[i]
 
-#     else:
-#         if arr[i + 1] > arr[i - 1]:
-#             arr[i + 1] = 0
-#             d[i] = d[i-1] + arr[i]
-#             cnt += 1
-#         else:
-#             arr[i] = 0
-#             d[i] = d[i - 1]
-#             cnt += 1
+    else:
+        if arr[i + 1] > arr[i - 1]:
+            arr[i + 1] = 0
+            d[i] = d[i-1] + arr[i]
+            cnt += 1
+        else:
+            arr[i] = 0
+            d[i] = d[i - 1]
+            cnt += 1
 
-# print(cnt)
+print(cnt)
 
 arr.reverse()
 
