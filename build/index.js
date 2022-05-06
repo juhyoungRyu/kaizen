14a1c5c52ccc109bc299bff037b8e02072a9aae3
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-06 16:49:18 +0900

    ts study

--- build/index.js
+++
@@ -1,8 +0,0 @@
-class Block {
-    constructor(data) {
-        this.data = data;
-    }
-    static hello() {
-        return "hi";
-    }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
-  }
-}
+import { init, exit } from "./myPackage";
+
+init();
