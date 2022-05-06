d6f854fe57f4e856c90f498836c709241f060fc6
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-06 19:17:02 +0900

    TypeScript is awesome ; '

---
+++ build/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = __importDefault(require("crypto"));
+class Block {
+  constructor(prevHash, height, data) {
+    this.prevHash = prevHash;
+    this.height = height;
+    this.data = data;
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash, height, data) {
+    const toHahs = `${prevHash}${height}${data}`;
+    return crypto_1.default.createHash("sha256").update(toHahs).digest("hex");
+  }
+}
+class BlockChain {
+  constructor() {
+    this.blocks = [];
+  }
+  getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  addBlock(data) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length, data);
+    this.blocks.push(newBlock);
+  }
+  getBlocks() {
+    return [...this.blocks];
+  }
+}
+const blockchain = new BlockChain();
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+console.log(blockchain.getBlocks());
--- src/index.ts
+++ src/index.ts
@@ -23,3 +23,31 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHahs).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(this.getPrevHash(), this.blocks.length, data);
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new BlockChain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
