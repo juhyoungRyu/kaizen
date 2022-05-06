009666f77835563470d6b80327541ed01fc5263c
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-06 17:42:21 +0900

    making block interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
 
-init();
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHahs = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHahs).digest("hex");
+  }
+}
