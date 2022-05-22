8364ac410db19c37027718bbedbd7ec3d772944d
juhyoungryu <thinkoutside2004@gmail.com>
2022-05-23 01:08:03 +0900

    1.4

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
 
 func main() {
-	fmt.Println("Hello World")
+	totalLenght, up := lenAndUpper("nico")
+	fmt.Println(totalLenght, up)
 }
