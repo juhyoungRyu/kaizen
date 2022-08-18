14fca5aeebd596e3e13cd6b450f38152bef6eb58
juhyoungRyu <thinkoutside2004@gmail.com>
2022-08-19 00:41:33 +0900

    Vanilla increase&decrease app rebuilding with Redux

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "Add") {
-    return count + 1;
-  } else if (action.type === "Minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
 
-add.addEventListener("click", () => {
-  countStore.dispatch({ type: "ADD" });
-});
+number.innerText = countStore.getState();
 
-add.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
-});
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-console.log(countStore.getState());
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
