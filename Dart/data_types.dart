af68573eea56d0224653642e49845e633409c738
juhyoungRyu <thinkoutside2004@gmail.com>
2023-01-01 16:20:08 +0900

    Dart : 자료형 공부중 - 배열

---
+++ Dart/data_types.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = 'juhyoung'; // 문자열
+  bool alive = true; // 논리형
+  int age = 12; // 정수형: num 파생
+  double money = 69.99; // 실수형: num 파생
+  num x = 12; // 숫자형, int와 double의 부모클래스
+  x = 1.1;
+
+  var numbers = [1, 2, 3, 4]; // list형 = 배열
+  List<int> numbers2 = [1, 2, 3, 4]; // 다르게 선언한 버전
+}
