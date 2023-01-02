8ff2b03a493d6857bf55dd6c72fae95ef41478f7
juhyoungRyu <thinkoutside2004@gmail.com>
2023-01-02 15:36:02 +0900

    Dart : 자료형 공부중 - 사전, 집합

--- Dart/data_types.dart
+++ Dart/data_types.dart
@@ -1,4 +1,6 @@
 void main() {
+  // Basic Data Types
+
   String name = 'juhyoung'; // 문자열
   bool alive = true; // 논리형
   int age = 12; // 정수형: num 파생
@@ -6,6 +8,45 @@ void main() {
   num x = 12; // 숫자형, int와 double의 부모클래스
   x = 1.1;
 
+  // Lists
   var numbers = [1, 2, 3, 4]; // list형 = 배열
-  List<int> numbers2 = [1, 2, 3, 4]; // 다르게 선언한 버전
+  var isFive = true;
+  List<int> numbers2 = [
+    1,
+    2,
+    3,
+    4,
+    if (isFive) 5, // Collection If
+  ]; // 명시적 타입 선언
+
+  numbers.add(1); // 같은 타입만 삽입 가능
+
+  // String Interpolation
+  var greeting = 'Hello everyone, my name is $name, and I\'m ${age + 1}';
+
+  // Collection For
+  var oldFriends = ['name1', 'name2'];
+  var newFriends = [
+    'name3',
+    'name4',
+    for (var friend in oldFriends) "💝 $friend", // Python For이랑 닮았다고 생각함
+  ];
+
+  // Maps
+  // Js의 Object, Python의 Dictionary와 비슷
+  var player = {
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  };
+
+  Map<String, Object> player2 = {
+    'name': 'nico',
+    'xp': 19.99,
+    'superpower': false,
+  }; // 명시적 타입 선언
+
+  // Sets
+  var setNum = {1, 2, 3, 4};
+  Set<int> setNum2 = {1, 2, 3, 4}; // 명시적 타입 선언
 }
